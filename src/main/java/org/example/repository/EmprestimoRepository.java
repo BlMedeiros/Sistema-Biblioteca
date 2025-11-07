ac8d85e2172e13c9546bcb8b0ad65faf4f9b8486
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-11-06 21:28:00 -0300

    feat: adiciona novos métodos ao EmprestimoRepository

--- src/main/java/org/example/repository/EmprestimoRepository.java
+++ src/main/java/org/example/repository/EmprestimoRepository.java
@@ -1,4 +1,38 @@
 package org.example.repository;
 
+import org.example.infra.Conexao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class EmprestimoRepository {
+
+    public void inserirEmprestimo(int idLivro, int idUsuario) throws SQLException {
+        String insertQuery = """
+            INSERT INTO emprestimos
+            (livro_id,usuario_id,data_emprestimo)
+            VALUES(?,?,?)
+            """;
+
+        try(Connection connection = Conexao.conectar();
+            PreparedStatement stmt = connection.prepareStatement(insertQuery)) {
+
+            stmt.setInt(1, idLivro);
+
+            stmt.setInt(2, idUsuario);
+
+            Date dataAtual = new Date(System.currentTimeMillis());
+            stmt.setDate(3, dataAtual);
+
+            int linhasAfetadas = stmt.executeUpdate();
+
+            if(linhasAfetadas > 0) {
+                System.out.println("Empréstimo Cadastrado com Sucesso!");
+            } else {
+                System.out.println("Não Foi Possível Cadastrar o Empréstimo!");
+            }
+        }
+    }
 }
