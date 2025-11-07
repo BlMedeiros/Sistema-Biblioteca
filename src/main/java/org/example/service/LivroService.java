71bd8b0dd698397a8a2635a9e491982a37ba900d
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-11-06 21:29:10 -0300

    feat: adiciona método de listagem e atualização de status na service

--- src/main/java/org/example/service/LivroService.java
+++ src/main/java/org/example/service/LivroService.java
@@ -3,6 +3,9 @@ package org.example.service;
 import org.example.model.Livro;
 import org.example.repository.LivroRepository;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class LivroService {
 
     LivroRepository livroRepository;
@@ -12,20 +15,41 @@ public class LivroService {
     }
 
     public void inserirLivro(String titulo, String autor, int ano) {
-        if(titulo.isBlank() || autor.isBlank() || ano <= 0) {
-            throw new RuntimeException("As Informações do Livro Não Podem Ser Nulas!");
-        }
+        if (titulo.isBlank() || autor.isBlank() || ano <= 0) {
+            System.err.println("As Informações do Livro Não Podem Ser Nulas!");
+        } else {
+            try {
+                Livro livro = new Livro(titulo, autor, ano);
 
+                livroRepository.inserirLivro(livro);
+            } catch (RuntimeException e) {
+                System.err.println("Ocorreu um Erro ao Inserir o Livro no Banco de Dados: " + e.getMessage());
+                e.printStackTrace();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public List<Livro> listarLivro() {
         try {
-            Livro livro = new Livro(titulo,autor,ano);
+            List<Livro> livroList = livroRepository.listarLivro();
 
-            livroRepository.inserirLivro(livro);
-        }catch(RuntimeException e) {
-            System.err.println("Ocorreu um Erro ao Inserir o Livro no Banco de Dados: " + e.getMessage());
-            e.printStackTrace();
-
-            throw new RuntimeException("Ocorreu um Erro Interno ao Cadastrar no Banco de Dados");
+            return livroList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+    }
 
+    public void atualizarStatusLivro(int idLivro) {
+        if(idLivro == 0 ) {
+            System.err.println("As Informações do Livro Não Podem Ser Nulas!");
+        } else {
+            try {
+                livroRepository.atualizarStatusLivro(idLivro,false);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
